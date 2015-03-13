7878ecc4c80893e003e2881a2d8e8646bc914032
Jawad <jawadnmd@gmail.com>
2015-03-12 21:31:40 -0400

    Menu Scene Example
    Just an example of a simple UI. Need to fix it up (particularly aspect
    size, pivots, and animations), as did this one really fast before a
    meeting. Good example for how our workspaces should be, too.

---
+++ Unity/DiabetesGame/Assets/jnWorkspace/UI Tutorial Assets/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	private Animator animator;
+	private CanvasGroup canvasGroup;
+	
+	public bool IsOpen {
+		get { return animator.GetBool("IsOpen"); }
+		set { animator.SetBool("IsOpen", value); }
+	}
+	
+	public void Awake() {
+		animator = GetComponent<Animator>();
+		canvasGroup = GetComponent<CanvasGroup>();
+		
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+	}
+
+	public void Update() {
+	/*
+		if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Open")) {
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = false;
+		}
+		else {
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = true;
+		}
+		*/
+	}
+}
