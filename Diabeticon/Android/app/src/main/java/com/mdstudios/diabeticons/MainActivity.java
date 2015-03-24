f68f9ee4c3105b43d8a2cdb2fc5899ca45fab61d
jawadmn <jawadmn@gmail.com>
2015-03-24 16:55:52 -0400

    Done with ListView of all the Diabeticons. Icons show in a neat little square, alongside their appropriately formatted title -> Can't click on them just yet

--- Diabeticon/Android/app/src/main/java/com/mdstudios/diabeticons/MainActivity.java
+++ Diabeticon/Android/app/src/main/java/com/mdstudios/diabeticons/MainActivity.java
@@ -1,16 +1,24 @@
 package com.mdstudios.diabeticons;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,6 +26,8 @@ public class MainActivity extends ActionBarActivity {
 
   // The Actionbar-replacement Toolbar that runs along the top of the screen
   Toolbar mToolbar;
+
+  ListView mListView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +40,20 @@ public class MainActivity extends ActionBarActivity {
       setSupportActionBar(mToolbar);
     }
 
+    // TODO: Check if there is saved data- if so, then don't redo everything unnecessarily
     // Set up the basic content
     initContent();
   }
 
   private void initContent() {
-    testAllImages();
+    // Grab the ListView, so can set it up
+    mListView = (ListView) findViewById(R.id.listview);
+
+    // Give it the appropriate adapter
+    IconListAdapter adapter = new IconListAdapter(this);
+    mListView.setAdapter(adapter);
+
+    //testAllImages();
   }
 
   // Prints out all the target images from assets to the log
@@ -87,5 +105,97 @@ public class MainActivity extends ActionBarActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  private class IconListAdapter extends BaseAdapter {
+    Context mContext;
+
+    String[] mTitles;             // The titles for each item
+    ArrayList<Drawable> mImages;  // All the images for each item
+
+    public IconListAdapter(Context context) {
+      this.mContext = context;
+
+      // Get all the necessary images and titles
+      AssetManager assetManager = context.getAssets(); // Necessary to access assets
+      try {
+        // Get all the file names
+        mTitles = assetManager.list("diabeticons"); // "diabeticons" = path
+
+        // Save all the names and images
+        mImages = new ArrayList<>();
+        for(int i = 0; i < mTitles.length; ++i) {
+          // TODO: Get and cache the appropriate image
+          // Cache the image for this item from its file path
+          String path = "diabeticons/" + mTitles[i];
+          Drawable d = Drawable.createFromStream(getAssets().open(path), null);
+          mImages.add(d);
+
+          // Get the simplified, displayable name (no .filetype, no nyNameIsFrankenstein)
+          String simplerName = mTitles[i].substring(0, mTitles[i].indexOf('.'));
+            // Looks for a lower case followed by an upper case, adds space between
+            // Source: http://stackoverflow.com/questions/4886091/insert-space-after-capital-letter
+          String simplerNameFinal = simplerName.replaceAll("(\\p{Ll})(\\p{Lu})","$1 $2");
+
+          // Replace the title with the easier-to-read title
+          mTitles[i] = simplerNameFinal;
+        }
+
+      } catch (IOException e) {
+        Log.e(LOGTAG, "There was an error! Error: " + e.toString());
+        e.printStackTrace();
+      }
+    }
+
+    @Override
+    public int getCount() {
+      return mTitles.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+      return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+      return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      View row = null;
+      ViewHolder holder;
+
+      if(convertView == null) {
+        // Then gotta set up this row for the first time
+        LayoutInflater inflater =
+            (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        row = inflater.inflate(R.layout.list_itemselect, parent, false);
+
+        // Create a ViewHolder to save all the different parts of the row
+        holder = new ViewHolder();
+        holder.icon = (ImageView) row.findViewById(R.id.icon);
+        holder.title = (TextView) row.findViewById(R.id.title);
+
+        // Make the row reuse the ViewHolder
+        row.setTag(holder);
+      }
+      else { // Otherwise, use the recycled view
+        row = convertView;
+        holder = (ViewHolder) row.getTag();
+      }
+
+      // Set the title and icon of this item according to the position
+      holder.title.setText(mTitles[position]);
+      holder.icon.setImageDrawable(mImages.get(position));
+
+      return row;
+    }
+
+    class ViewHolder{
+      public ImageView icon;
+      public TextView title;
+    }
   }
 }
