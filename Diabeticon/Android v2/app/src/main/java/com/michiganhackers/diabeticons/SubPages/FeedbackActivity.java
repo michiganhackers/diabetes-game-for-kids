321fa42c3f01dbbbb7a0b29629529746749bb81b
jawadmn <jawadmn@gmail.com>
2015-10-21 18:08:48 -0400

    > Feedback page is now up layout-wise and "works" (still need to conenct the send button)

--- Diabeticon/Android v2/app/src/main/java/com/michiganhackers/diabeticons/SubPages/FeedbackActivity.java
+++ Diabeticon/Android v2/app/src/main/java/com/michiganhackers/diabeticons/SubPages/FeedbackActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.michiganhackers.diabeticons.R;
 
@@ -12,11 +15,12 @@ import com.michiganhackers.diabeticons.R;
  */
 public class FeedbackActivity extends ActionBarActivity {
     Toolbar mToolbar;
+    EditText mInputEditText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send);
+        setContentView(R.layout.activity_feedback);
 
         // Add the toolbar
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -24,5 +28,21 @@ public class FeedbackActivity extends ActionBarActivity {
             setSupportActionBar(mToolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        // Cache the EditText for later use
+        mInputEditText = (EditText) findViewById(R.id.input);
+
+        // Set up the send button
+        findViewById(R.id.btn_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the text from the input
+                String input = mInputEditText.getText().toString();
+
+                // For now, just toast it
+                Toast.makeText(FeedbackActivity.this, "Input: " + input, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 }
