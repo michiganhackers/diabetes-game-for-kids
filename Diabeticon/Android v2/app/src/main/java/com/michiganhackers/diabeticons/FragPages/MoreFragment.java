7302378eac021b62a9cabece7078e04e1b41e56b
jawadmn <jawadmn@gmail.com>
2015-10-21 14:17:17 -0400

    > Added majority of necessary UI strings to strings.xml > Made "More" page with realistic layout (has dummy icons, clicking on each row only pops up a toast)

--- Diabeticon/Android v2/app/src/main/java/com/michiganhackers/diabeticons/FragPages/MoreFragment.java
+++ Diabeticon/Android v2/app/src/main/java/com/michiganhackers/diabeticons/FragPages/MoreFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.michiganhackers.diabeticons.R;
 
@@ -15,7 +17,42 @@ import com.michiganhackers.diabeticons.R;
 public class MoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.tab_1,container,false);
+        View v =inflater.inflate(R.layout.frag_more,container,false);
+
+        v.findViewById(R.id.layout_about)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        choseAbout();
+                    }
+                });
+        v.findViewById(R.id.layout_feedback)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        choseFeedback();
+                    }
+                });
+        v.findViewById(R.id.layout_share)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        choseShareApp();
+                    }
+                });
+
         return v;
+    }
+
+    private void choseAbout() {
+        Toast.makeText(getActivity(), "Chose about page", Toast.LENGTH_SHORT).show();
+    }
+
+    private void choseFeedback() {
+        Toast.makeText(getActivity(), "Chose feedback page", Toast.LENGTH_SHORT).show();
+    }
+
+    private void choseShareApp() {
+        Toast.makeText(getActivity(), "Chose share page", Toast.LENGTH_SHORT).show();
     }
 }
